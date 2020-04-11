a23b30ad7d0be075593e747b31e2604960efac28
Brandon Ewing <brandon.ewing@warningg.com>
2020-04-11 18:41:40 -0500

    Add docstring to static source

--- pkg/fanout/static.go
+++ pkg/fanout/static.go
@@ -8,6 +8,7 @@ func (s *staticFanout) Receivers() ([]string, error) {
 	return s.destinations, nil
 }
 
+// NewStaticFanout returns a fanout to a static list of strings
 func NewStaticFanout(destinations []string) (Fanout, error) {
 	return &staticFanout{
 		destinations: destinations,
